77940cf65b5cc2474afff669ca91f3ac52cf48c5
anonymousboy67 <aashishad67@gmail.com>
2024-06-06 06:09:51 +0545

    first commit

---
+++ app/cabins/page.js
@@ -0,0 +1,16 @@
+export default async function Cabins(){
+
+    const res=await fetch('https://jsonplaceholder.typicode.com/users');
+    const data=await res.json();
+
+    return(
+        <div>
+            <h1>Cabin Pages</h1>
+            <ul>
+                {data.map((user)=>(
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
---
+++ app/components/Navigation.js
@@ -0,0 +1,18 @@
+
+import Link from "next/link";
+
+export default function Navigation(){
+    return(
+        <ul>
+            <li>
+                <Link href='/cabins'>Cabins</Link>
+            </li>
+            <li>
+                <Link href='/about'>about</Link>
+            </li>
+            <li>
+                <Link href='/account'>account</Link>
+            </li>
+        </ul>
+    )
+}
